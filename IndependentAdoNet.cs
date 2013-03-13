8ff763a18f651858dd27078090c96d1cd2f1ee1b
Gökhan Türedi <turedi.gokhan@gmail.com>
2013-03-13 16:20:34 +0200

    ilk kayıt

---
+++ IndependentAdoNet.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+
+namespace ClientApp
+{
+    class IndependentAdoNet:IDisposable
+    {
+        private IDbConnection _connection;
+        private IDbCommand _command;
+
+        public IndependentAdoNet(IDbConnection connection)
+        {
+            _connection = connection;
+            
+            connection.Open();
+
+            _command = _connection.CreateCommand();
+        }
+
+        public int ExecuteQuery(string query)
+        {
+            _command.CommandText = query;
+
+            var affectedRows = _command.ExecuteNonQuery();
+            _command.Parameters.Clear();
+
+            return affectedRows;
+        }
+
+        public DataTable GetData(string query)
+        {
+            var resultTable = new DataTable();
+
+            _command.CommandText = query;
+
+            using (var reader = _command.ExecuteReader())
+                resultTable.Load(reader);
+
+            _command.Parameters.Clear();
+
+            return resultTable;
+        }
+
+        public void Dispose()
+        {
+            if (_connection != null) 
+                _connection.Close();
+        }
+    }
+}
